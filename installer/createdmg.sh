57de0a7082078aa9b244588181f258d1a25ae5fd
John G <jnga@users.noreply.github.com>
2015-02-23 15:15:29 -0500

    Add an installer.

---
+++ installer/createdmg.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Create a disk image file for the installer.
+#
+# Usage: createdmg.sh {version-number}
+#
+# Example: ./createdmg.sh 1.0
+
+if [ $# -le 0 ] 
+    then 
+		echo "This script must be run with a version number." 
+	    echo -e "Usage: $0 {version-number}"
+		exit 1
+	fi
+
+PROJNAME=ReadableMail
+version=$1
+dmgname=$PROJNAME-$version.dmg
+sparseimage=$dmgname.sparseimage
+
+# Remove any old files.
+rm $dmgname 2> /dev/null
+rm $sparseimage 2> /dev/null
+
+# Create a sparse disk image.
+hdiutil create -size 100M -type SPARSE -volname "$PROJNAME" \
+-fs HFS+ $sparseimage
+
+# Attach the sparse image and parse its device name from the output.
+devicename=$(hdiutil attach $sparseimage | \
+egrep '^/dev/' | sed 1q | awk '{print $1}')
+
+# Copy the installer into the sparse image.
+cp -R ./build/$PROJNAME.mpkg /Volumes/$PROJNAME/
+
+# Detach the sparse image. 
+hdiutil detach -force $devicename
+
+# Convert the sparse image into a distributable disk image file.
+hdiutil convert "$sparseimage" -format UDZO -o "$dmgname" -ov \
+-imagekey zlib-level=9
+
+# Clean up.
+rm $sparseimage 2> /dev/null
